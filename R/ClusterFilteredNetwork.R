6e85c35eef75f4daa9cd520d4e64b62e99595e80
g-4455 <grantms4455@gmail.com>
2025-06-03 14:31:27 -0600

    Remove cor_matrix parameter from process_correlation_edges since it's global (may need to undo)

--- R/ClusterFilteredNetwork.R
+++ R/ClusterFilteredNetwork.R
@@ -11,7 +11,6 @@ pepgene <- function(peps) {
 #'
 #' This function processes correlation edges from a given correlation matrix
 #'
-#' @param cor_matrix A correlation matrix.
 #' @param mode A string specifying the graph mode. Default is "lower".
 #'
 #' @return A data frame of correlation edges.
@@ -20,8 +19,8 @@ pepgene <- function(peps) {
 #' @examples
 #' process_correlation_edges(cor_matrix)
 # Function to process correlation edges
-process_correlation_edges <- function(cccn_matrix, mode="lower") {
-  g <- igraph::graph_from_adjacency_matrix(cccn_matrix, mode=mode, diag=FALSE, weighted="Weight")
+process_correlation_edges <- function(mode="lower") {
+  g <- igraph::graph_from_adjacency_matrix(mode=mode, diag=FALSE, weighted="Weight")
   edges <- data.frame(igraph::as_edgelist(g))
   edges$Weight <- igraph::edge_attr(g)[[1]]
   edges$edgeType <- "correlation"
