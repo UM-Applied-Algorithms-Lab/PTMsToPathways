a6d3e2209a8460acd766541e9fd6a1c448932b34
maddie-moffett <mnrmoffett@gmail.com>
2025-06-04 09:58:12 -0600

    fix example for find ppi edges

--- R/FindPPIEdges.R
+++ R/FindPPIEdges.R
@@ -139,7 +139,7 @@ make_gm_input <- function(cccn_matrix) {
 #' @export
 #'
 #' @examples
-#' find_ppi_edges(cccn_matrix, gmfilepath = 'genemania/genemania-interactions.txt')
+#' find_ppi_edges(cccn_matrix, gmfilepath = '~/CCCN_CFN_Tools/data/genemania/genemania-interactions.txt')
 find_ppi_edges <- function(cccn_matrix, gmfilepath = "genemania-interactions.txt") {
 
   #test if nodenames already exists and therefore if they ran optional step 3
