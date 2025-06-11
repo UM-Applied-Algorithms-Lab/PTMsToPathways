1aea4429ab731c7256589f88b6632f998b90de4d
g-4455 <grantms4455@gmail.com>
2025-06-11 14:19:30 -0600

    Remove weight comparisson between cccn_matrix and ppi_edges. Add cccn_matrix weights column to cfn_network

--- R/ClusterFilteredNetwork.R
+++ R/ClusterFilteredNetwork.R
@@ -8,25 +8,30 @@
 #' @export
 #' @examples
 #' cccn.cfn.tools:::ex.ClusterFilteredNetwork()
-ClusterFilteredNetwork <- function(accuracy = 0.000001) {
+ClusterFilteredNetwork <- function() {
   #Error Catch
   if(!exists("cccn_matrix")) stop("cccn_matrix not found. Consider running MakeCorrelationNetwork(keeplength) to make one.")
   if(!exists("ppi_network")) stop("ppi_network not found. Consider looking at step 3 & 4 in https://um-applied-algorithms-lab.github.io/CCCN_CFN_Tools/articles/CoClusterCorrelationNetwork.html to make one.")
   
   #Loop through ppi_network and assign every row that matches genenames to an include vector
   include <- c()
+  weights <- c() #CFN
   for(a in 1:length(rownames(ppi_network))){
     #Initilization
     Gene1 <- ppi_network[a, 1] #Get gene from row a, first col
     Gene2 <- ppi_network[a, 2] #Get gene from row a, second col
-    ppi_weight <- as.numeric(ppi_network[a, 3]) #Get the weight of row a
+    cmw <- cccn_matrix[Gene1, Gene2]
     
-    #If the ppi_weight equals the value that appears in the cccn_matrix for the same genes, add the row number to include
-    if(all.equal(ppi_weight, cccn_matrix[Gene1, Gene2], tolerance=accuracy) == TRUE) include[length(include)+1] <- a #all.equal does not return false
+    #If the same edge in the cccn_matrix has any nonzero correlation, add it to the matrix
+    if(cmw != 0){
+      include[length(include)+1] <- a
+      weights[length(include)+1] <- cmw
+      }
   }
   
   #Assign
   cfn_network <- ppi_network[include, ] #cfn network should only take rows that have been identified by include
+  cfn_network$cor_weight <- cmw
   if(nrow(cfn_network) == 0) stop("No common edges between PPI edges and cccn_matrix") #Throw error if no intersection found
   assign("cfn_network", cfn_network, envir = .GlobalEnv) #Cluster Filtered Network
 }
