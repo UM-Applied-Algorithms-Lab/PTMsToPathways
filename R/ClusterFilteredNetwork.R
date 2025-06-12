32c1fdb4ebf16d810fa7f838e8f502b037b59d4e
maddie-moffett <mnrmoffett@gmail.com>
2025-06-12 11:31:29 -0600

    finish adding estimated runtimes; update website

--- R/ClusterFilteredNetwork.R
+++ R/ClusterFilteredNetwork.R
@@ -2,9 +2,9 @@
 
 #' ClusterFilteredNetwork
 #'
-#' This function finds the intersection between a list of PPI edges and a matrix (which it interprets as a weighted adjacency matrix). Assigns a cfn_network variable to the global environment, which is a list of the common edges between the PPI edges and cccn_matrix. 
-#' @param accuracy How close a PPI can weight be to a correlation weight to accept the edge. It is recommeneded that genemania users do not increase this parameter as it defaults to the maximum sig figs for a genemania egde weight.  
-#' 
+#' This function finds the intersection between a list of PPI edges and a matrix (which it interprets as a weighted adjacency matrix). Assigns a cfn_network variable to the global environment, which is a list of the common edges between the PPI edges and cccn_matrix.
+#' @param accuracy How close a PPI can weight be to a correlation weight to accept the edge. It is recommeneded that genemania users do not increase this parameter as it defaults to the maximum sig figs for a genemania egde weight.
+#'
 #' @export
 #' @examples
 #' cccn.cfn.tools:::ex.ClusterFilteredNetwork()
@@ -12,7 +12,7 @@ ClusterFilteredNetwork <- function() {
   #Error Catch
   if(!exists("cccn_matrix")) stop("cccn_matrix not found. Consider running MakeCorrelationNetwork(keeplength) to make one.")
   if(!exists("ppi_network")) stop("ppi_network not found. Consider looking at step 3 & 4 in https://um-applied-algorithms-lab.github.io/CCCN_CFN_Tools/articles/CoClusterCorrelationNetwork.html to make one.")
-  
+
   #Loop through ppi_network and assign every row that matches genenames to an include vector
   include <- c()
   weights <- c() #CFN
@@ -20,13 +20,13 @@ ClusterFilteredNetwork <- function() {
     #Initilization
     Gene1 <- ppi_network[a, 1] #Get gene from row a, first col
     Gene2 <- ppi_network[a, 2] #Get gene from row a, second col
-    if(Gene1 %in% rownames(cccn_matrix) && Gene2 %in% colnames(cccn_matrix)){
-      cmw <- cccn_matrix[Gene1, Gene2] #Get the weight 
+    if(Gene1 %in% colnames(cccn_matrix) && Gene2 %in% rownames(cccn_matrix)){
+      cmw <- cccn_matrix[Gene1, Gene2] #Get the weight
       if(cmw != 0){ #If the weight is nonzero
         include[length(include)+1] <- a
         weights[length(weights)+1] <- cmw
   }}}
-  
+
   #Assign
   cfn_network <- ppi_network[include, ] #cfn network should only take rows that have been identified by include
   if(nrow(cfn_network) == 0) stop("No common edges between PPI edges and cccn_matrix") #Throw error if no intersection found
