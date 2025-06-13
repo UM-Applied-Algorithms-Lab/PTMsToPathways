5092ebafbc9d114441ec3248fe58b5c55fbb10cb
g-4455 <grantms4455@gmail.com>
2025-06-13 11:11:04 -0600

    Refine roxy skeleton

--- R/PathwayCrosstalkNetwork.R
+++ R/PathwayCrosstalkNetwork.R
@@ -1,34 +1,17 @@
-#'Helper function to take information from a bioplanet file and format it in the desired way. Assumes that the function was downloaded from excel. 
+#' Converts Bioplanet pathways from (<https://tripod.nih.gov/bioplanet/>)  into a list of pathways whose elements are the genes in each pathway. Edge weights are either the PTM Cluster Weight or according to the Jaccard Similarity.
+#' 
+#' @param filename The name of the bioplanet pathway .csv file    
+#' @return No clue (The result is a matrix with pathway names in columns and individual clusters as rows.)
+#' @export
 #'
-#'@param filename The name of the bioplanet pathway .csv file    
-#'@return A formatted version of the bioplanet csv file
-#'@export
-#'
-#'@examples
-#'print("TO DO")
-bioplanet.innit <- function(fn){
-  print("Unused; may delete later")
-}
-
-#'
-#'
-#'@param filename The name of the bioplanet pathway .csv file    
-#'@return No clue (The result is a matrix with pathway names in columns and individual clusters as rows.)
-#'@export
-#'
-#'@examples
-#'print("TO DO")
-PathwayCrosstalkNetwork <- function(filename){
+#' @examples
+#' print("TO DO")
+PathwayCrosstalkNetwork <- function(filename, PCNname = "pcn_matrix"){
+  #Error catch if file/directory is wrong
   if(!file.exists(filename)) stop(paste(filename, "not found. Plese check your working directory."))
   
   #Read file in, converts to dataframe like with rows like: PATHWAY_ID | PATHWAY_NAME | GENE_ID | GENE_SYMBOL
   bioplanet <- read.csv(filename, stringsAsFactors = F) 
   
-  ###
-  #Add excel fix here? 
-  ###
-  
-  #Return
-  return(bioplanet) 
   
 }
