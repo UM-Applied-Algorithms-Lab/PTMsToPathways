74cd4fe4b3b1a9898f70df60cd6bd5ad343f84b7
g-4455 <grantms4455@gmail.com>
2025-06-13 12:46:19 -0600

    Solution

--- R/PathwayCrosstalkNetwork.R
+++ R/PathwayCrosstalkNetwork.R
@@ -15,8 +15,24 @@ PathwayCrosstalkNetwork <- function(file, PCNname = "pcn_matrix"){
   
   #Turn bioplanet into a list of pathways. Pathways are character vectors comprised of gene names 
   pathways.list <- plyr::dlply(bioplanet, plyr::.(PATHWAY_NAME)) #Turn the bioplanet .csv into a list of data frames. Each data frame stores genes with the same PATHWAY_ID 
-  pathways.list <- lapply(bioplanet, `[`, -c(1:3)) #Removes PATHWAY_ID | PATHWAY_NAME | GENE_ID from every single data frame in the list
+  pathways.list <- lapply(pathways.list, `[`, -c(1:3)) #Removes PATHWAY_ID | PATHWAY_NAME | GENE_ID from every single data frame in the list
   pathways.list <- lapply(pathways.list, unlist, use.names = FALSE) #Since data frames are 1 row, turn data frames into character vectors
+
+  
+#Jaccard Similarity#
+  #Create a matrix whose [i, j] values are the intersection between the genes in pathway i and j
+  matrix.common <- matrix(NA, nrow = length(pathways.list), ncol = length(pathways.list)) 
+  
+  #Populate matrix
+  for (i in 1:length(pathways.list)) {
+    for (j in (i+1):length(pathways.list)) { #Populate diagonal
+      pathway.intersect <- intersect(unlist(pathways.list[i]), unlist(pathways.list[j]))
+      len <- length(pathway.intersect)
+      if(len == 0) {
+        matrix.common[i, j] <- len #Number of genes pathway i and j share
+        matrix.common[j, i] <- len #Since matrix is symmetrical
+      }
+  }}
   
   
 }
