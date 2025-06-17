f1bc8b340cb7f53c6f656a68eb8f7ca4675d6165
maddie-moffett <mnrmoffett@gmail.com>
2025-06-17 10:43:24 -0600

    fix nodenames reading and update vignette

--- R/processGMedgefile.R
+++ R/processGMedgefile.R
@@ -10,12 +10,13 @@
 #'
 #' @examples
 #' cccn.cfn.tools::ex.processGMedgefile("path/to/edgefile", "path/to/nodetable", ex.nodenames, ex.gm.network)
-processGMedgefile <- function(gm.edgefile.path, gm.nodetable.path, nodenames, gm.network.name = "gm_network"){
-  edgetable <- utils::read.csv(gm.edgefile.path, header = TRUE)   # read the edgefile
-  nodetable <- utils::read.csv(gm.nodetable.path, header = TRUE)  # read the nodetable
+processGMedgefile <- function(gm.edgefile.path, gm.nodetable.path, db_nodes.path, gm.network.name = "gm_network"){
+  edgetable <- utils::read.csv(gm.edgefile.path, header = TRUE)        # read the edgefile
+  nodetable <- utils::read.csv(gm.nodetable.path, header = TRUE)       # read the nodetable
+  nodenames <- utils::read.table(db_nodes.path, header = FALSE)[[1]]   # read the nodenames file
 
   edgetable <- subset(edgetable, select = c(name, raw.weights))   # only look at the name and raw.weights columns
-  nodetable <- subset(nodetable, select = c(name, query.term))    # only look at the names (GM ID) and query.term (real names) columnts
+  nodetable <- subset(nodetable, select = c(name, query.term))    # only look at the names (GM ID) and query.term (real names) columns
 
   edgetable$Gene.1 <- 'null'   # make new columns!
   edgetable$Gene.2 <- 'null'
@@ -26,17 +27,17 @@ processGMedgefile <- function(gm.edgefile.path, gm.nodetable.path, nodenames, gm
   edgetable$Gene.2 <- sapply(split_names, function(x)x[2])                                             # take the first thing; first ID name
   edgetable$Gene.2 <- sapply(edgetable$Gene.2, function(x)nodetable$query.term[nodetable$name == x])   # turn the ID into the gene name!
 
-  edges <- edgetable[, c("Gene.1", "Gene.2", "raw.weights")]             # sort into a new table with only our information and in the order we want
+  edges <- edgetable[, c("Gene.1", "Gene.2", "raw.weights")]                # sort into a new table with only our information and in the order we want
 
-  colnames(edges) <- c("Gene.1", "Gene.2", "GM.raw.weights")             # rename so when put with everything, clearer where came from
+  colnames(edges) <- c("Gene.1", "Gene.2", "GM.raw.weights")                # rename so when put with everything, clearer where came from
 
-  split_weights <- strsplit(edges$raw.weights, "\\|")                    # split up the weights because they have between 1 and SIX-GODDAMN-TEEN possible weights
+  split_weights <- strsplit(edges$GM.raw.weights, "\\|")                    # split up the weights because they have between 1 and SIX-GODDAMN-TEEN possible weights
 
-  weights.list <- lapply(split_weights, function(x)as.numeric(x))        # make into a number :)
-  edges$raw.weights <- sapply(weights.list, function(x)max(x))           # take the maximum? THIS CAN BE CHANGED LATER! I picked max for vibes, but can be mean, sum, etc
+  weights.list <- lapply(split_weights, function(x)as.numeric(x))           # make into a number :)
+  edges$GM.raw.weights <- sapply(weights.list, function(x)max(x))           # take the maximum? THIS CAN BE CHANGED LATER! I picked max for vibes, but can be mean, sum, etc
 
-  #max_num <- max(edges$raw.weights)                                     # this is code that can be uncommented
-  #edges$raw.weights <- sapply(edges$raw.weights, function(x)x/max_num)  # adjusts all of the weights to still have a max of 1; to be used with the sum above ^^^^
+  #max_num <- max(edges$GM.raw.weights)                                     # this is code that can be uncommented
+  #edges$GM.raw.weights <- sapply(edges$raw.weights, function(x)x/max_num)  # adjusts all of the weights to still have a max of 1; to be used with the sum above ^^^^
 
   keep <- edges$Gene.1 %in% nodenames & edges$Gene.2 %in% nodenames      # which rows are we keeping
   all.edges <- edges[keep,]                                              # copy 'em over
