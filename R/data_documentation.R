4730dfe06b265fd1d888b0665e578b0c420b1781
maddie-moffett <mnrmoffett@gmail.com>
2025-06-18 15:06:54 -0600

    initialize documentation for rda files

---
+++ R/data_documentation.R
@@ -0,0 +1,210 @@
+#' Bioplanet Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.bioplanet)
+#' head(ex.bioplanet)
+"ex.bioplanet"
+
+#' CoCluster Correlation Network
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.cccn_matrix)
+#' head(ex.cccn_matrix)
+"ex.cccn_matrix"
+
+#' Ptms Data
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source "mass spectroscopy data"
+#' @examples
+#' data(ex.eu.sp.sed.ptms.data)
+#' head(ex.eu.sp.sed.ptms.data)
+"ex.eu.sp.sed.ptms.data"
+
+#' Pathways List Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.pathways.list)
+#' head(ex.pathways.list)
+"ex.pathways.list"
+
+#' GeneMANIA Edges Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.gm.edges)
+#' head(ex.gm.edges)
+"ex.gm.edges"
+
+#' GeneMANIA Network Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.gm.network)
+#' head(ex.gm.network)
+"ex.gm.network"
+
+#' GeneMANIA Nodes Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.gm.nodes)
+#' head(ex.gm.nodes)
+"ex.gm.nodes"
+
+#' GeneMANIA Empty Edges Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.gm_edges)
+#' head(ex.gm_edges)
+"ex.gm_edges"
+
+#' List of Common Clusters Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.list.common)
+#' head(ex.list.common)
+"ex.list.common"
+
+#' Nodenames Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.nodenames)
+#' head(ex.nodenames)
+"ex.nodenames"
+
+#' PPI Network Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.ppi_network)
+#' head(ex.ppi_network)
+"ex.ppi_network"
+
+#' Correlation Matrix Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.ptm.correlation.matrix)
+#' head(ex.ptm.correlation.matrix)
+"ex.ptm.correlation.matrix"
+
+#' PTM Table Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.ptmtable)
+#' head(ex.ptmtable)
+"ex.ptmtable"
+
+#' TSNE matrices Example
+#'
+#' (description)
+#'
+#' @format :
+#' \describe{
+#'   \item{}{}
+#'   ...
+#' }
+#' @source source info (or `"manually created"`)
+#' @examples
+#' data(ex.tsne.matrices)
+#' head(ex.tsne.matrices)
+"ex.tsne.matrices"
+
