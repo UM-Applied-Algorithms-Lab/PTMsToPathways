43999d2c261cd2476cf5ea14afc79933b0eea829
maddie-moffett <mnrmoffett@gmail.com>
2025-06-19 13:49:36 -0600

    finish data documentation

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -4,14 +4,18 @@
 #'
 #' @format Dataframe with 100 rows and 4 columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{PATHWAY_ID}{ID of the pathway in bioplanet}
+#'   \item{PATHWAY_NAME}{Common name of the pathway}
+#'   \item{GENE_ID}{ID of the gene in bioplanet}
+#'   \item{GENE_SYMBOL}{Common name of the gene}
 #' }
-#' @source source info (or `"manually created"`)
+#' @source \url{https://tripod.nih.gov/bioplanet/}
 #' @examples
 #' data(ex.bioplanet)
 #' head(ex.bioplanet)
 "ex.bioplanet"
+
+
 
 #' CoCluster Correlation Network
 #'
@@ -20,12 +24,15 @@
 #' @format Square matrix with 19 rows and columns:
 #' \describe{
 #'   \item{rows/columns}{Gene}
+#'   \item{values}{Strength of correlation}
 #' }
 #' @source "created by MakeCorrelationNetwork"
 #' @examples
 #' data(ex.cccn_matrix)
 #' head(ex.cccn_matrix)
 "ex.cccn_matrix"
+
+
 
 #' Pathways List Example
 #'
@@ -41,6 +48,8 @@
 #' head(ex.pathways.list)
 "ex.pathways.list"
 
+
+
 #' Full GeneMANIA Edges Example
 #'
 #' Network of interactions from the GeneMANIA database.
@@ -48,16 +57,15 @@
 #' @format Dataframe of 601,552 rows and 10 columns:
 #' \describe{
 #'   \item{data.type}{Type of interaction; typically co-expression}
-#'   \item{highlight}{UNKNOWN}
-#'   \item{interaction}{UNKNOWN}
+#'   \item{highlight}{Internal flag}
+#'   \item{interaction}{Type of interaction}
 #'   \item{name}{The names of the two genes interacting as well as the data type}
 #'   \item{networks}{Source of information}
-#'   \item{normalized.max.weight}{UNKNOWN}
+#'   \item{normalized.max.weight}{raw.weights scaled to be consistent}
 #'   \item{raw.weights}{A number weighting the strength of the interaction}
-#'   \item{selected}{UNKNOWN}
-#'   \item{shared.interaction}{UNKNOWN}
-#'   \item{shared.name}{UNKNOWN}
-#'   ...
+#'   \item{selected}{Internal flag}
+#'   \item{shared.interaction}{Type of interaction}
+#'   \item{shared.name}{The names of the two genes interacting as well as the data type}
 #' }
 #' @source \url{https://apps.cytoscape.org/apps/genemania}
 #' @examples
@@ -65,14 +73,17 @@
 #' head(ex.gm.edges)
 "ex.gm.edges"
 
+
+
 #' Full GeneMANIA Network Example
 #'
 #' Network of relationships between genes based on information from the GeneMANIA database.
 #'
 #' @format Dataframe of 601,552 rows and 3 columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{Gene.1}{The gene interacting with Gene.2}
+#'   \item{Gene.2}{The gene interacting with Gene.1}
+#'   \item{raw.weights}{Strength of the interaction}
 #' }
 #' @source \url{https://apps.cytoscape.org/apps/genemania}
 #' @examples
@@ -80,14 +91,29 @@
 #' head(ex.gm.network)
 "ex.gm.network"
 
+
+
 #' GeneMANIA Nodes Example
 #'
 #' Dataframe from GeneMANIA relating GeneMANIA IDs to common gene names.
 #'
 #' @format Dataframe of 2762 rows and 16 columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{annotation.name}{Note about the gene}
+#'   \item{annotations}{Processes associated with this gene according to Gene Ontology}
+#'   \item{Ensemble.Gene.ID}{ID of the gene in Ensemble}
+#'   \item{Entrez.Gene.ID}{ID of the gene in Entrez}
+#'   \item{gene.name}{Common name fo the gene}
+#'   \item{log.score}{Association strength to the set of entered genes on a log scale}
+#'   \item{name}{Name of the gene in GeneMANIA}
+#'   \item{node.type}{Why the node is in the list}
+#'   \item{query.term}{What was searched for; typically the common name of the gene}
+#'   \item{RefSeq.Protein.ID}{ID of the gene in RefSeq}
+#'   \item{score}{Predicted association strength to the set of entered genes}
+#'   \item{selected}{Internal flag}
+#'   \item{shared.name}{Name of the gene in GeneMANIA}
+#'   \item{Synonym}{Gene synonym in another database}
+#'   \item{Uniprot.ID}{ID of the gene in Uniprot}
 #' }
 #' @source \url{https://apps.cytoscape.org/apps/genemania}
 #' @examples
@@ -95,14 +121,18 @@
 #' head(ex.gm.nodes)
 "ex.gm.nodes"
 
+
+
 #' GeneMANIA Empty Edges Example
 #'
 #' Empty GeneMANIA structure. TODO: fix.
 #'
 #' @format Dataframe of 0 rows and 4 columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{Gene.1}{The gene interacting with Gene.2}
+#'   \item{Gene.2}{The gene interacting with Gene.1}
+#'   \item{Weight}{Strength of the interaction}
+#'   \itm{Network.group}{Type of interaction}
 #' }
 #' @source \url{https://genemania.org}
 #' @examples
@@ -110,14 +140,15 @@
 #' head(ex.gm_edges)
 "ex.gm_edges"
 
+
+
 #' List of Common Clusters Example
 #'
 #' (description)
 #'
-#' @format List of 12 variables:
+#' @format List of 12 lists:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{list}{Common cluster between the three distance metrics}
 #' }
 #' @source source info (or `"manually created"`)
 #' @examples
@@ -125,20 +156,23 @@
 #' head(ex.list.common)
 "ex.list.common"
 
+
+
 #' Nodenames Example
 #'
 #' List of gene names in dataset.
 #'
 #' @format Dataframe of 10 rows and 1 column:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{Gene.Names}{Name of the gene}
 #' }
-#' @source source info (or `"manually created"`)
+#' @source "created by MakeDBInput"
 #' @examples
 #' data(ex.nodenames)
 #' head(ex.nodenames)
 "ex.nodenames"
+
+
 
 #' PPI Network Example
 #'
@@ -146,14 +180,18 @@
 #'
 #' @format Dataframe of 1 row and 4 columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{Gene.1}{The gene interacting with Gene.2}
+#'   \item{Gene.2}{The gene interacting with Gene.1}
+#'   \item{Weight}{Strength of the interaction}
+#'   \itm{edgeType}{Type of interaction between Gene.1 and Gene.2}
 #' }
-#' @source source info (or `"manually created"`)
+#' @source "created by FindPPIEdges"
 #' @examples
 #' data(ex.ppi_network)
 #' head(ex.ppi_network)
 "ex.ppi_network"
+
+
 
 #' Correlation Matrix Example
 #'
@@ -161,14 +199,16 @@
 #'
 #' @format Square matrix of 100 rows and columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{rows and columns}{Ptm names}
+#'   \item{values}{Strength of correlation}
 #' }
-#' @source source info (or `"manually created"`)
+#' @source "created by MakeClusterList"
 #' @examples
 #' data(ex.ptm.correlation.matrix)
 #' head(ex.ptm.correlation.matrix)
 "ex.ptm.correlation.matrix"
+
+
 
 #' PTM Table Example
 #'
@@ -176,25 +216,29 @@
 #'
 #' @format Dataframe of 100 rows and 70 columns:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{rows}{Post-translational modifications}
+#'   \item{columns}{Experimental condition}
+#'   \item{values}{Expression as found by mass spectrometry}
 #' }
-#' @source source info (or `"manually created"`)
+#' @source "manually created"
 #' @examples
 #' data(ex.ptmtable)
 #' head(ex.ptmtable)
 "ex.ptmtable"
 
+
+
 #' TSNE matrices Example
 #'
 #' List of of lists of dataframes of ptm positions in 3D space to show their relationships with each other based on distance.
 #'
-#' @format List of 3 lists of 6 (Euclidean and SED) or 88 (Spearman) dataframes of varying sizes:
+#' @format List of 3 Distance metrics (Euclidean, Spearman, and the average of Spearman and Euclidean (SED)) and the clusters that form when they are run through tsne:
 #' \describe{
-#'   \item{}{}
-#'   ...
+#'   \item{Euclidean}{A list of 6 clusters of ptms based on their Euclidean distance and positional distance determined by tsne}
+#'   \item{Spearman}{A list of 6 clusters of ptms based on their Spearman dissimilarity and positional distance determined by tsne}
+#'   \item{SED}{A list of 6 clusters of ptms based on the average of their Euclidean distance and Spearman Dissimilarity and positional distance determined by tsne}
 #' }
-#' @source source info (or `"manually created"`)
+#' @source "created by MakeClusterList"
 #' @examples
 #' data(ex.tsne.matrices)
 #' head(ex.tsne.matrices)
