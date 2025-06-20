004ce3399d7a0cc2b480c7bfe3b638abf7d82854
maddie-moffett <mnrmoffett@gmail.com>
2025-06-20 11:11:29 -0600

    change is.na -> !is.data.frame avoid issues with is.na of every row and column entry

--- R/FindPPIEdges.R
+++ R/FindPPIEdges.R
@@ -17,20 +17,20 @@
 #' cccn.cfn.tools:::ex.FindPPIEdges(ex.cccn_matrix, ppi.network.name = "ex.ppi_network")
 FindPPIEdges <- function(string.edges = NA, db_filepaths = c(), gm.network = NA, ppi.network.name = "ppi.network") {
 
-  if (is.na(string.edges) && is.na(gm.network) && (length(db_filepaths) == 0)){
+  if (!is.data.frame(string.edges) && !is.data.frame(gm.network) && (length(db_filepaths) == 0)){
     stop("No data input.")
   }
 
   ppi.network <- data.frame()
 
-  if(!is.na(string.edges)){
+  if(is.data.frame(string.edges)){
     if(!is.data.frame(string.edges)){
       stop("string.edges is not a dataframe; invalid formatting")
     }
     ppi.network <- rbind(ppi.network, string.edges)
   }
 
-  if (!is.na(gm.network)){
+  if (is.data.frame(gm.network)){
     if(!is.data.frame(string.edges)){
       stop("gm.network is not a dataframe; invalid formatting")
     ppi.network <- rbind(ppi.network, gm.network)
