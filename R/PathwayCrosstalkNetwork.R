d753bfa65a8407a881d41994621b48ced48be527
g-4455 <grantms4455@gmail.com>
2025-06-26 16:04:12 -0600

    Fix last commit

--- R/PathwayCrosstalkNetwork.R
+++ R/PathwayCrosstalkNetwork.R
@@ -19,8 +19,8 @@ ClusterPathwayEvidence <- function(cluster, pathway, p.list){
   #Calculate CPE score and add it to sigma
   for(k in 1:length(pathway)){
     #A space needs to be appended
-    temp <- sum(pathway[k] == cluster.format)) #Numerator: The amount of times Gene k appears in cluster
-    temp <- temp / sum(sapply(p.list, function(x) sum(pathway[[k]] == x))) #Divide temp by the number of times Gene k appears in pathways in the pathway list
+    temp <- sum(pathway[k] == cluster.format) #Numerator: The amount of times Gene k appears in cluster
+    temp <- temp / sum(sapply(p.list, function(x) pathway[[k]] %in% x)) #Divide temp by the number of times Gene k appears in pathways in the pathway list
     sigma[k] <- temp #Assign this value to sigma[k]
   }
 
