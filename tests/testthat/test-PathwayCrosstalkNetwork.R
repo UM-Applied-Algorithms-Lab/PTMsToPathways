af4c251973083b0a908a9cf8f7e0c74166dde66e
g-4455 <grantms4455@gmail.com>
2025-06-30 14:10:13 -0600

    I forgot a line

--- tests/testthat/test-PathwayCrosstalkNetwork.R
+++ tests/testthat/test-PathwayCrosstalkNetwork.R
@@ -10,6 +10,7 @@ test_that("Row 20 Jaccard Weight", {expect_equal(edgelist_123a$Jaccard_weight[[2
 test_that("Row 66 Jaccard Weight", {expect_equal(edgelist_123a$Jaccard_weight[[66]], 0.4285714,tolerance=0.00001)})
 
 #CPE Evidence
+writeLines(""); writeLines("Testing PTP interaction evidence weights")
 test_that("Row 15 PTP interaction evidence", {expect_equal(edgelist_123a$PTP_inter_evidence[[15]], 0.1666667,tolerance=0.00001)})
 test_that("Row 35 PTP interaction evidence", {expect_equal(edgelist_123a$PTP_inter_evidence[[35]], 0.2083333,tolerance=0.00001)})
 test_that("Row 56 PTP interaction evidence", {expect_equal(edgelist_123a$PTP_inter_evidence[[56]], 1,tolerance=0.00001)})
