ec90e7cd3b0f6c80855a2d553c5e8b3b1a8f88db
maddie-moffett <mnrmoffett@gmail.com>
2025-07-07 14:38:49 -0600

    make inputs matter; add screenshot to vigfigs

--- R/CytoscapeGraphing.R
+++ R/CytoscapeGraphing.R
@@ -11,7 +11,13 @@
 #'
 #' @examples
 #' GraphCFN(ex.cfn)
-GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.tools"){
+GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.tools",
+                     background.color = '#fcf3cf', edge.label.color = '#17202a', edge.line.color = '#abb2b9', node.border.color = '#145a32', node.label.color = '#145a32', node.color = "#a9dfbf",
+                     default.font = "Times New Roman", node.font.size = 12, edge.font.size = 8,
+                     edge.line.style = 'SOLID', source.arrow = 'NONE', target.arrow = 'NONE', node.shape = "OCTAGON",
+                     node.size = 50, edge.width = 2, border.width = 1,
+                     edge.opacity = 175, edge.label.opacity = 255, border.opacity = 255, node.label.opacity = 255, node.fill.opacity = 255
+                     ){
 
   if(!(exists("RCy3"))){                      # check if RCy3 is libraried
     if(system.file(package="RCy3") == ""){    # check if RCy3 is installed at all
@@ -20,8 +26,6 @@ GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.
     }
     library("RCy3")                           # library
   }
-
-
 
 
 
@@ -45,54 +49,52 @@ GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.
 
 
 
-
-
   # CUSTOMIZATION FROM HERE ON OUT
 
-  setBackgroundColorDefault('#EDC8FF')              # self explanatory
+  # STANDARD: if want to change; do separately
 
-  # edge stuff
-  setEdgeFontFaceDefault()                          # set font of edge
-  setEdgeFontSizeDefault()                          # set font size of edge
-  setEdgeOpacityDefault(175)                        # 0 - 255
-  setEdgeLabelColorDefault()                        # self explanatory
-  setEdgeLabelOpacityDefault()                      # 0 - 255
-  setEdgeLineStyleDefault()                         # "PARALLEL_LINES", "MARQUEE_EQUAL", "DOT", "EQUAL_DASH", "LONG_DASH", "CONTIGUOUS_ARROW", "MARQUEE_DASH", "DASH_DOT", "BACKWARD_SLASH", "FORWARD_SLASH", "VERTICAL_SLASH", "SOLID", "SEPARATE_ARROW", "MARQUEE_DASH_DOT", "ZIGZAG", "SINEWAVE"
-  setEdgeLineWidthDefault()                         # Initial Default 2
-  setEdgeOpacityDefault()                           # 0 - 255
-  setEdgeColorDefault('#899499')                    # self explanatory
-  # arrow stuff
-  setEdgeSourceArrowColorDefault('#899499')         # self explanatory
-  setEdgeTargetArrowColorDefault('#899499')         # self explanatory
-  setEdgeSourceArrowShapeDefault()                  # "DELTA", "DIAMOND", "OPEN_CIRCLE", "CIRCLE", "OPEN_HALF_CIRCLE", "CROSS_OPEN_DELTA", "DELTA_SHORT_1", "CROSS_DELTA", "OPEN_DELTA", "OPEN_DIAMOND"
-  setEdgeTargetArrowShapeDefault()                  # "DIAMOND_SHORT_1", "DELTA_SHORT_2", "OPEN_SQUARE", "NONE", "SQUARE", "DIAMOND_SHORT_2", "T", "HALF_BOTTOM", "HALF_TOP", "ARROW_SHORT", "HALF_CIRCLE"
-
-  # node stuff
-  setNodeShapeDefault("VEE")                        # "RECTANGLE", "VEE", "OCTAGON", "ELLIPSE", "PARALLELOGRAM", "DIAMOND", "HEXAGON", "ROUND_RECTANGLE", "TRIANGLE"
-  setNodeSizeDefault(50)                            # self explanatory
-  # border stuff
-  setNodeBorderColorDefault('#899499')              # self explanatory
-  setNodeBorderOpacityDefault(255)                  # 0 - 255
-  setNodeBorderWidthDefault(0)                      # Initial Default 0
-  # else
-  setNodeColorDefault("#89D0F5")                    # self explanatory
-  setNodeFillOpacityDefault()                       # 0 - 255
-  setNodeFontFaceDefault()                          #
-  setNodeFontSizeDefault()                          #
-  setNodeSizeDefault()                              # Initial Default 50
-  setNodeLabelColorDefault('#899499')               # self explanatory
-  setNodeLabelOpacityDefault()                      # 0 - 255
+  setEdgeSourceArrowColorDefault(edge.line.color)   # set color of arrow from source
+  setEdgeTargetArrowColorDefault(edge.line.color)   # set color of arrow to target
   setNodeLabelPositionDefault("C", "C", "c", 0, 0)  # What part of the node label is aligned "C", "NW", "N", "NE", "E", "SE", "S", "SW", "W"
                                                     # to what part of the node graphic       "C", "NW", "N", "NE", "E", "SE", "S", "SW", "W"
                                                     # "l", "r", "c"
                                                     # amount offset in the x direction
                                                     # amount offset in the y direction
-
   # mapVisualProperty(visual.property, table.column, mapping.type, table.column.values, visual.prop.values)
   # visual.properties can be found with ?mapVisualProperty (thank god) some useful ones are: "node label", "node fill color", "Edge Target Arrow Shape", many more
   # table.column shows which column of the table is being mapped (id for the node label) or analyzed (interaction for arrow shape) for this property
   # mapping.type is 'c' for continuous (you know what continuous means); 'd' for discrete (set determined vals as in arrow shape and node color); 'p' for passthrough (each is different? used for node label)
   # table.column.values determine which entries for that column give you which VVVV
   # visual.prop.values give the property type assigned to the specified ^^^^^
+
+  # CUSTOMIZED: user inputs can change the following:
+
+  # colors
+  setBackgroundColorDefault(background.color)       # set color of background
+  setEdgeLabelColorDefault(edge.label.color)        # set color of edge label
+  setEdgeColorDefault(edge.line.color)              # set color of edge
+  setNodeBorderColorDefault(node.border.color)      # set color of node border
+  setNodeLabelColorDefault(node.label.color)        # set color of node title
+  setNodeColorDefault(node.color)                   # set interior color of node
+  # fonts
+  setEdgeFontFaceDefault(default.font)              # set font of edge
+  setNodeFontFaceDefault(default.font)              # set font of node title (Initial Default UNKNOWN (not TNR or Arial))
+  setEdgeFontSizeDefault(edge.font.size)            # set font size of edge (Initial Default 12)
+  setNodeFontSizeDefault(node.font.size)            # set font size fo node title (Initial Default 12)
+  # shape/style
+  setEdgeLineStyleDefault(edge.line.style)          # "PARALLEL_LINES", "MARQUEE_EQUAL", "DOT", "EQUAL_DASH", "LONG_DASH", "CONTIGUOUS_ARROW", "MARQUEE_DASH", "DASH_DOT", "BACKWARD_SLASH", "FORWARD_SLASH", "VERTICAL_SLASH", "SOLID", "SEPARATE_ARROW", "MARQUEE_DASH_DOT", "ZIGZAG", "SINEWAVE"
+  setEdgeSourceArrowShapeDefault(source.arrow)      # "DELTA", "DIAMOND", "OPEN_CIRCLE", "CIRCLE", "OPEN_HALF_CIRCLE", "CROSS_OPEN_DELTA", "DELTA_SHORT_1", "CROSS_DELTA", "OPEN_DELTA", "OPEN_DIAMOND"
+  setEdgeTargetArrowShapeDefault(target.arrow)      # "DIAMOND_SHORT_1", "DELTA_SHORT_2", "OPEN_SQUARE", "NONE", "SQUARE", "DIAMOND_SHORT_2", "T", "HALF_BOTTOM", "HALF_TOP", "ARROW_SHORT", "HALF_CIRCLE"
+  setNodeShapeDefault(node.shape)                   # "RECTANGLE", "VEE", "OCTAGON", "ELLIPSE", "PARALLELOGRAM", "DIAMOND", "HEXAGON", "ROUND_RECTANGLE", "TRIANGLE"
+  # size
+  setNodeSizeDefault(node.size)                     # set size of node; height and width assumed the same
+  setEdgeLineWidthDefault(edge.width)               # set edge line thickness; Initial Default 2
+  setNodeBorderWidthDefault(border.width)           # set border thickness; Initial Default 0
+  # opacity
+  setEdgeOpacityDefault(edge.opacity)               # set opacity of edge; 0 - 255 w 0 --> translucent
+  setEdgeLabelOpacityDefault(edge.label.opacity)    # set opacity of edge label; 0 - 255 w 0 --> translucent
+  setNodeBorderOpacityDefault(border.opacity)       # set opacity of border of node; 0 - 255 w 0 --> translucent
+  setNodeFillOpacityDefault(node.fill.opacity)      # set opacity of interior color of node; 0 - 255 w 0 --> translucent
+  setNodeLabelOpacityDefault(node.label.opacity)    # set opacity of title of node; 0 - 255 w 0 --> translucent
 
 }
