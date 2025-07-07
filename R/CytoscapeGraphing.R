3e2c135b57256a5b7fb26f359e4e350588099c2a
maddie-moffett <mnrmoffett@gmail.com>
2025-07-07 15:20:04 -0600

    add all parameters to GraphCfn; document and build everything

--- R/CytoscapeGraphing.R
+++ R/CytoscapeGraphing.R
@@ -6,13 +6,41 @@
 #' @param Network.title Desired title for the created Cytoscape Network
 #' @param Network.collection Desired name for the collection created on Cytoscape in which the network will reside
 #'
+#' @param background.color Hex code of background color of graph; defaults to '#fcf3cf'
+#' @param edge.label.color Hex code of edge label color of graph; defaults to '#17202a'
+#' @param edge.line.color Hex code of edge line color of graph; defaults to '#abb2b9'
+#' @param node.border.color Hex code of node border color of graph; defaults to '#145a32'
+#' @param node.label.color Hex code of node label color of graph; defaults to '#145a32'
+#' @param node.fill.color Hex code of node fill color of graph; defaults to '#a9dfbf'
+#'
+#' @param default.font Font style of edge and node names; defaults to "Times New Roman"
+#' @param node.font.size Font size of the node name; defaults to 12
+#' @param edge.font.size Font size of the edge name; defaults to 8
+#'
+#' @param edge.line.style Type of edge style; defaults to "SOLID"; options include: "PARALLEL_LINES", "MARQUEE_EQUAL", "DOT", "EQUAL_DASH", "LONG_DASH", "CONTIGUOUS_ARROW", "MARQUEE_DASH", "DASH_DOT", "BACKWARD_SLASH", "FORWARD_SLASH", "VERTICAL_SLASH", "SOLID", "SEPARATE_ARROW", "MARQUEE_DASH_DOT", "ZIGZAG", "SINEWAVE"
+#' @param source.arrow Type of arrow coming from the source gene; defaults to "NONE"; options include: "DELTA", "DIAMOND", "OPEN_CIRCLE", "CIRCLE", "OPEN_HALF_CIRCLE", "CROSS_OPEN_DELTA", "DELTA_SHORT_1", "CROSS_DELTA", "OPEN_DELTA", "OPEN_DIAMOND", "DIAMOND_SHORT_1", "DELTA_SHORT_2", "OPEN_SQUARE", "NONE", "SQUARE", "DIAMOND_SHORT_2", "T", "HALF_BOTTOM", "HALF_TOP", "ARROW_SHORT", "HALF_CIRCLE"
+#' @param target.arrow Type of arrow going to the target gene; defaults to "NONE"; options include: "DELTA", "DIAMOND", "OPEN_CIRCLE", "CIRCLE", "OPEN_HALF_CIRCLE", "CROSS_OPEN_DELTA", "DELTA_SHORT_1", "CROSS_DELTA", "OPEN_DELTA", "OPEN_DIAMOND", "DIAMOND_SHORT_1", "DELTA_SHORT_2", "OPEN_SQUARE", "NONE", "SQUARE", "DIAMOND_SHORT_2", "T", "HALF_BOTTOM", "HALF_TOP", "ARROW_SHORT", "HALF_CIRCLE"
+#' @param node.shape Shape of the node; defaults to "OCTAGON"; options include: "RECTANGLE", "VEE", "OCTAGON", "ELLIPSE", "PARALLELOGRAM", "DIAMOND", "HEXAGON", "ROUND_RECTANGLE", "TRIANGLE"
+#'
+#' @param node.size Size of the node; defaults to 50. PLEASE NOTE: width and height can be changed independently using RCy3 directly (first run lockNodeDimensions(FALSE) and then setNodeWidthDefault() and setNodeHeightDefault())
+#' @param edge.width Width of the edge line; defaults to 2
+#' @param border.width Width of the border of the node; defaults to 1
+#'
+#' @param edge.opacity Opacity of the edge line on a scale of 0 - 255 with 0 being transparent; defaults to 175
+#' @param edge.label.opacity Opacity of the edge label on a scale of 0 - 255 with 0 being transparent; defaults to 255
+#' @param border.opacity Opacity of the node border on a scale of 0 - 255 with 0 being transparent; defaults to 255
+#' @param node.label.opacity Opacity of the node label on a scale of 0 - 255 with 0 being transparent; defaults to 255
+#' @param node.fill.opacity Opacity of the node fill on a scale of 0 - 255 with 0 being transparent; defaults to 255
+#'
 #' @return A cytoscape graph of the cluster filtered network
 #' @export
 #'
 #' @examples
-#' GraphCFN(ex.cfn)
+#' cytoscape.pic <- system.file("extdata/cytoscape_graph.png", package = "cccn.cfn.tools")
+#' #GraphCFN(ex.cfn)
+#' magick::image_read(cytoscape.pic)
 GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.tools",
-                     background.color = '#fcf3cf', edge.label.color = '#17202a', edge.line.color = '#abb2b9', node.border.color = '#145a32', node.label.color = '#145a32', node.color = "#a9dfbf",
+                     background.color = '#fcf3cf', edge.label.color = '#17202a', edge.line.color = '#abb2b9', node.border.color = '#145a32', node.label.color = '#145a32', node.fill.color = '#a9dfbf',
                      default.font = "Times New Roman", node.font.size = 12, edge.font.size = 8,
                      edge.line.style = 'SOLID', source.arrow = 'NONE', target.arrow = 'NONE', node.shape = "OCTAGON",
                      node.size = 50, edge.width = 2, border.width = 1,
@@ -74,13 +102,13 @@ GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.
   setEdgeLabelColorDefault(edge.label.color)        # set color of edge label
   setEdgeColorDefault(edge.line.color)              # set color of edge
   setNodeBorderColorDefault(node.border.color)      # set color of node border
-  setNodeLabelColorDefault(node.label.color)        # set color of node title
-  setNodeColorDefault(node.color)                   # set interior color of node
+  setNodeLabelColorDefault(node.label.color)        # set color of node name
+  setNodeColorDefault(node.fill.color)              # set interior color of node
   # fonts
   setEdgeFontFaceDefault(default.font)              # set font of edge
-  setNodeFontFaceDefault(default.font)              # set font of node title (Initial Default UNKNOWN (not TNR or Arial))
+  setNodeFontFaceDefault(default.font)              # set font of node name (Initial Default UNKNOWN (not TNR or Arial))
   setEdgeFontSizeDefault(edge.font.size)            # set font size of edge (Initial Default 12)
-  setNodeFontSizeDefault(node.font.size)            # set font size fo node title (Initial Default 12)
+  setNodeFontSizeDefault(node.font.size)            # set font size of node name (Initial Default 12)
   # shape/style
   setEdgeLineStyleDefault(edge.line.style)          # "PARALLEL_LINES", "MARQUEE_EQUAL", "DOT", "EQUAL_DASH", "LONG_DASH", "CONTIGUOUS_ARROW", "MARQUEE_DASH", "DASH_DOT", "BACKWARD_SLASH", "FORWARD_SLASH", "VERTICAL_SLASH", "SOLID", "SEPARATE_ARROW", "MARQUEE_DASH_DOT", "ZIGZAG", "SINEWAVE"
   setEdgeSourceArrowShapeDefault(source.arrow)      # "DELTA", "DIAMOND", "OPEN_CIRCLE", "CIRCLE", "OPEN_HALF_CIRCLE", "CROSS_OPEN_DELTA", "DELTA_SHORT_1", "CROSS_DELTA", "OPEN_DELTA", "OPEN_DIAMOND"
@@ -95,6 +123,6 @@ GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.
   setEdgeLabelOpacityDefault(edge.label.opacity)    # set opacity of edge label; 0 - 255 w 0 --> translucent
   setNodeBorderOpacityDefault(border.opacity)       # set opacity of border of node; 0 - 255 w 0 --> translucent
   setNodeFillOpacityDefault(node.fill.opacity)      # set opacity of interior color of node; 0 - 255 w 0 --> translucent
-  setNodeLabelOpacityDefault(node.label.opacity)    # set opacity of title of node; 0 - 255 w 0 --> translucent
+  setNodeLabelOpacityDefault(node.label.opacity)    # set opacity of name of node; 0 - 255 w 0 --> translucent
 
 }
