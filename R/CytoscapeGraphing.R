652f5069ea5b727322c14f4b0e0b9d8a13bd428f
maddie-moffett <mnrmoffett@gmail.com>
2025-07-07 12:09:57 -0600

    continue annotations and find suitable defaults

--- R/CytoscapeGraphing.R
+++ R/CytoscapeGraphing.R
@@ -13,6 +13,20 @@
 #' GraphCFN(ex.cfn)
 GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.tools"){
 
+  if(!(exists("RCy3"))){                      # check if RCy3 is libraried
+    if(system.file(package="RCy3") == ""){    # check if RCy3 is installed at all
+      BiocManager::install(version='devel')   # The following initializes usage of Bioc devel
+      BiocManager::install("RCy3")            # install!
+    }
+    library("RCy3")                           # library
+  }
+
+
+
+
+
+  # ACTUAL CODE AND DATA PROCESSING
+
   genes <- unique(c(cfn$Gene.1, cfn$Gene.2))
 
   cfn.edges <- data.frame(matrix(data = 0, nrow = length(rownames(cfn)), ncol = 4))
@@ -29,6 +43,10 @@ GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.
 
   createNetworkFromDataFrames(cfn.nodes, cfn.edges, title = Network.title, collection = Network.collection)
 
+
+
+
+
   # CUSTOMIZATION FROM HERE ON OUT
 
   setBackgroundColorDefault('#EDC8FF')              # self explanatory
@@ -37,35 +55,33 @@ GraphCfn <- function(cfn, Network.title = "cfn", Network.collection = "cccn.cfn.
   setEdgeFontFaceDefault()                          # set font of edge
   setEdgeFontSizeDefault()                          # set font size of edge
   setEdgeOpacityDefault(175)                        # 0 - 255
-  setEdgeLabelColorDefault()                        #
-  setEdgeLabelDefault()                             #
-  setEdgeLabelOpacityDefault()                      #
-  setEdgeLineStyleDefault()                         #
-  setEdgeLineWidthDefault()                         #
-  setEdgeOpacityDefault()                           #
+  setEdgeLabelColorDefault()                        # self explanatory
+  setEdgeLabelOpacityDefault()                      # 0 - 255
+  setEdgeLineStyleDefault()                         # "PARALLEL_LINES", "MARQUEE_EQUAL", "DOT", "EQUAL_DASH", "LONG_DASH", "CONTIGUOUS_ARROW", "MARQUEE_DASH", "DASH_DOT", "BACKWARD_SLASH", "FORWARD_SLASH", "VERTICAL_SLASH", "SOLID", "SEPARATE_ARROW", "MARQUEE_DASH_DOT", "ZIGZAG", "SINEWAVE"
+  setEdgeLineWidthDefault()                         # Initial Default 2
+  setEdgeOpacityDefault()                           # 0 - 255
   setEdgeColorDefault('#899499')                    # self explanatory
   # arrow stuff
-  setEdgeSourceArrowColorDefault()                  #
-  setEdgeSourceArrowShapeDefault()                  #
-  setEdgeTargetArrowColorDefault()                  #
-  setEdgeTargetArrowShapeDefault()                  #
+  setEdgeSourceArrowColorDefault('#899499')         # self explanatory
+  setEdgeTargetArrowColorDefault('#899499')         # self explanatory
+  setEdgeSourceArrowShapeDefault()                  # "DELTA", "DIAMOND", "OPEN_CIRCLE", "CIRCLE", "OPEN_HALF_CIRCLE", "CROSS_OPEN_DELTA", "DELTA_SHORT_1", "CROSS_DELTA", "OPEN_DELTA", "OPEN_DIAMOND"
+  setEdgeTargetArrowShapeDefault()                  # "DIAMOND_SHORT_1", "DELTA_SHORT_2", "OPEN_SQUARE", "NONE", "SQUARE", "DIAMOND_SHORT_2", "T", "HALF_BOTTOM", "HALF_TOP", "ARROW_SHORT", "HALF_CIRCLE"
 
   # node stuff
   setNodeShapeDefault("VEE")                        # "RECTANGLE", "VEE", "OCTAGON", "ELLIPSE", "PARALLELOGRAM", "DIAMOND", "HEXAGON", "ROUND_RECTANGLE", "TRIANGLE"
   setNodeSizeDefault(50)                            # self explanatory
   # border stuff
-  setNodeBorderColorDefault()                       #
-  setNodeBorderOpacityDefault()                     #
-  setNodeBorderWidthDefault()                       #
+  setNodeBorderColorDefault('#899499')              # self explanatory
+  setNodeBorderOpacityDefault(255)                  # 0 - 255
+  setNodeBorderWidthDefault(0)                      # Initial Default 0
   # else
-  setNodeColorDefault()                             #
-  setNodeFillOpacityDefault()                       #
+  setNodeColorDefault("#89D0F5")                    # self explanatory
+  setNodeFillOpacityDefault()                       # 0 - 255
   setNodeFontFaceDefault()                          #
   setNodeFontSizeDefault()                          #
-  setNodeHeightDefault()                            #
-  setNodeLabelColorDefault()                        #
-  setNodeLabelOpacityDefault()                      #
-  setNodeWidthDefault()                             #
+  setNodeSizeDefault()                              # Initial Default 50
+  setNodeLabelColorDefault('#899499')               # self explanatory
+  setNodeLabelOpacityDefault()                      # 0 - 255
   setNodeLabelPositionDefault("C", "C", "c", 0, 0)  # What part of the node label is aligned "C", "NW", "N", "NE", "E", "SE", "S", "SW", "W"
                                                     # to what part of the node graphic       "C", "NW", "N", "NE", "E", "SE", "S", "SW", "W"
                                                     # "l", "r", "c"
