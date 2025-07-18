9302963acc9de143007cb31ee783139a0a5ab526
g-4455 <grantms4455@gmail.com>
2025-07-18 10:13:13 -0600

    fix MCN

--- tests/testthat/test-MakeCorrelationNetwork.R
+++ tests/testthat/test-MakeCorrelationNetwork.R
@@ -1,18 +1,18 @@
 #Run function
-MakeCorrelationNetwork(ex.clusters.list, ex.ptm.correlation.matrix, 1, clusters.name = "common_123a", cccn.name = "cccn_123a") 
+MakeCorrelationNetwork(ex.common.clusters, ex.ptm.correlation.matrix, ptm.cccn.name = "ptms_123a", gene.cccn.name = "gene_123a") 
 
-#Test common clusters
-test_that("Cluster 1, PTM 2", {expect_equal(common_123a$`Cluster 1`[[2]], "ABCC1 ubi K1502")})
-test_that("Cluster 6, PTM 1", {expect_equal(common_123a$`Cluster 6`[[1]], "ABCA3 p S683")})
-test_that("Cluster 7, PTM 1", {expect_equal(common_123a$`Cluster 7`[[1]], "ABL1 p Y393; ABL2 p Y439")})
-test_that("Cluster 8, PTM 2", {expect_equal(common_123a$`Cluster 8`[[2]], "ABLIM1 p Y373")})
 
-#Test cccn_matrix for correctness
-test_that("Testing cccn_matrix ABCA1, ABCC1", {expect_equal(cccn_123a["ABCA1","ABCC1"], 10)})
-test_that("Testing cccn_matrix ACKR3, ABCC1", {expect_equal(cccn_123a["ACKR3","ABCC1"], 9.6)})
-test_that("Testing cccn_matrix ABLIM1, ABHD11", {expect_equal(cccn_123a["ABLIM1","ABHD11"], 5.8)})
-test_that("Testing cccn_matrix AATF, ABCD3", {expect_equal(cccn_123a["AATF","ABCD3"], 1)})
+#Test ptms, since PTMs are just a filtered version of the correlation matrix and thats tested already, only 2 tests
+test_that("Testing ptms matrix HSPA1B ubi K597, PKP4 p Y1156", {expect_equal(ptms_123a["HSPA1B ubi K597","PKP4 p Y1156"], 0.4285714, tolerance=0.00001)})
+
+
+#Test gene matrix for correctness
+test_that("Testing gene network diagionals are 0", {expect_equal(gene_123a[43, 43], 0)})
+test_that("Testing gene network CTTN, SEC16A", {expect_equal(gene_123a["CTTN", "SEC16A"], 60.94232, tolerance=0.00001)})
+test_that("Testing gene network ARAP2, OAT", {expect_equal(gene_123a["ARAP2", "OAT"], 0)})
+test_that("Testing gene network FKBP4, NSFL1C", {expect_equal(gene_123a["FKBP4", "NSFL1C"], 1.628571, tolerance=0.00001)})
+
 
 #Clean Up
-rm("common_123a", envir = .GlobalEnv)
-rm("cccn_123a", envir = .GlobalEnv)
+rm("ptms_123a", envir = .GlobalEnv)
+rm("gene_123a", envir = .GlobalEnv)
