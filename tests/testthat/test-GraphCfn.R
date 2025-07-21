b5920b54cc981b1624844cf534e035dc064b164f
maddie-moffett <mnrmoffett@gmail.com>
2025-07-21 14:44:40 -0600

    fix error catch for GraphCfn

--- tests/testthat/test-GraphCfn.R
+++ tests/testthat/test-GraphCfn.R
@@ -1,3 +1,7 @@
- test_that("Not connected to cytoscape", {
-  expect_error(GraphCfn(ex.cfn, ex.ptmtable), "Could not connect to Cytoscape. Please ensure the Cytoscape app is open and running. Wait until fully loaded, then run RCy3::cytoscapePing() to verify connection.")
- })
+test_that("Not connected to cytoscape", {
+   err <- tryCatch(
+     GraphCfn(ex.cfn, ex.ptmtable),
+     error = function(e) e
+   )
+   expect_equal(err$message, "Could not connect to Cytoscape. Please ensure the Cytoscape app is open and running. Wait until fully loaded, then run RCy3::cytoscapePing() to verify connection.")
+   })
