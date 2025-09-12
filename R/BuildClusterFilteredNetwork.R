0ea87e5b944cbdb08cae81690fe2fceac31b2568
Gram-S <grantms4455@gmail.com>
2025-09-11 19:07:08 -0600

    Remove assigns from CFN and clean roxygen skeleton

--- R/BuildClusterFilteredNetwork.R
+++ R/BuildClusterFilteredNetwork.R
@@ -1,27 +1,15 @@
 #' Build PPI Network
 #'
 #' Uses all inputted database information on protein-protein interactions in order to create a combined PPI-network.
-#'
-#' @param stringdb.edges Data frame of consisting of the network of interactions from the genes of study pulled from the STRINGdb database
-#' @param genemania.edges GeneMANIA network of protein-protein interactions from the genes of study; defaults to NA
-#' @param db.filepaths A vector of paths to the additional ppi network files; defaults to an empty vector
-#' @param ppi.network.name Desired name for the output protein-protein interaction network using all entered database input; defaults to ppi.network
-#'
-#' @return A dataframe representing how strongly proteins are known to interact based on previous research contained within provided database networks
-#' @export
-#'
-#' @examples
-#' ex.string <- ex.stringdb.edges
-#' ex.gm <- ex.genemania.edges
-#' #' Filter PPI Network by Known Clusters
-#'
 #' All edges in the PPI network are checked against the cocluster correlation network to ensure existence and a non-zero weight.
 #' If either of these conditions are not met, then it will be removed from the list of PPI edges.
 #' This new, cluster filtered network is then assigned to the global namespace.
-#'
-#' @param gene.cccn A matrix showing strength of relationships between proteins using the common clusters between the three distance metrics (Euclidean, Spearman, and Combined (SED))
-#' @param ppi.network A dataframe representing how strongly proteins are known to interact based on previous research contained within provided database networks
-#' @param cfn.name Desired name of the output cluster filtered network
+#' 
+#' @param stringdb.edges Data frame of consisting of the network of interactions from the genes of study pulled from the STRINGdb database
+#' @param genemania.edges GeneMANIA network of protein-protein interactions from the genes of study; defaults to NA
+#' @param kinsub.edges 
+#' @param gene.cccn.edges
+#' @param db.filepaths A vector of paths to the additional ppi network files; defaults to an empty vector
 #'
 #' @return A version of ppi.network with only the edges that exist in gene.cccn and have non-zero weights
 #'
@@ -30,7 +18,7 @@
 #' ClusterFilteredNetwork(ex.gene.cccn, ex.ppi.network, cfn.name = "ex.cfn")
 #' utils::head(ex.cfn)
 
-BuildClusterFilteredNetwork <- function(stringdb.edges, genemania.edges, kinsub.edges, gene.cccn.edges, db.filepaths = c(), ppi.network.name = "combined.PPIs", cfn.name = "cfn", returndata=TRUE) {
+BuildClusterFilteredNetwork <- function(stringdb.edges, genemania.edges, kinsub.edges, gene.cccn.edges, db.filepaths = c()) {
   # Combine PPIs from different databases
   # First Normalize Weights
   stringdb.edges$Weight <- 100 * stringdb.edges$Weight / max(stringdb.edges$Weight, na.rm = TRUE) #  this returns a range of 0 to 100
@@ -47,8 +35,6 @@ BuildClusterFilteredNetwork <- function(stringdb.edges, genemania.edges, kinsub.
   # Combine both (removing redundant rows if needed)
   cfn <- rbind(cfn1, cfn2)
   cfn <- unique(cfn)
-  assign(ppi.network.name, combined.PPIs, envir = .GlobalEnv)
-  assign(cfn.name, cfn, envir = .GlobalEnv)    # assign :)
-  if (returndata==TRUE) {return(list(combined.PPIs, cfn))}
+  return(list(combined.PPIs, cfn))
 }
 #______________________________________________________________________________________________________________________________
