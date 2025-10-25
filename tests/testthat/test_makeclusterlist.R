d9d9e018cf8bb028a45319d945c0b705f2ed038d
Lucy <lgw2@uw.edu>
2025-10-25 08:36:17 -0600

    fix cluster test

--- tests/testthat/test_makeclusterlist.R
+++ tests/testthat/test_makeclusterlist.R
@@ -2,8 +2,8 @@ test_that("MakeClusterList() gives right answer", {
     set.seed(25)
 
     # data to check clusters
-    clust1 <- list(c("EML4 p Y453", "GLUL p Y180", "PDLIM5 p Y251", "CD99L2 p Y243", "RDH10 p Y126", "GFPT1 p Y553", "CRK p Y251"))
-    clust2 <- list(c("SDC1 p Y309", "ARHGAP32 p Y1537", "ALK p Y1096", "APLP2 p Y755", "TIAL1 p Y140", "SNRPN p Y15; SNRPB p Y15"))
+    clust1 <- list("EML4 p Y453", "GLUL p Y180", "PDLIM5 p Y251", "CD99L2 p Y243", "RDH10 p Y126", "GFPT1 p Y553", "CRK p Y251")
+    clust2 <- list("SDC1 p Y309", "ARHGAP32 p Y1537", "ALK p Y1096", "APLP2 p Y755", "TIAL1 p Y140", "SNRPN p Y15; SNRPB p Y15")
 
     clusterlist.data <- MakeClusterList(ex_small_ptm_table, keeplength = 2, toolong = 3.5)
     common.clusters <- clusterlist.data[[1]]
@@ -15,11 +15,11 @@ test_that("MakeClusterList() gives right answer", {
     
     # check that the cluster that contains GLUL p Y180 is correct
     clust1_result <- unname(common.clusters[sapply(common.clusters, function(x) "GLUL p Y180" %in% x)])
-    setequal(clust1_result, clust1)
+    expect_setequal(clust1_result[[1]], clust1)
 
     # check that the cluster that contains SNRPN p Y15; SNRPB p Y15 is correct
     clust2_result <- unname(common.clusters[sapply(common.clusters, function(x) "SNRPN p Y15; SNRPB p Y15" %in% x)])
-    setequal(clust2_result, clust2)
+    expect_setequal(clust2_result[[1]], clust2)
 
     # check total clusters and total number of PTMs in clusters
     expect_equal(length(common.clusters),  132)
