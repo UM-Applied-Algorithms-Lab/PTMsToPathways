bfd4ae61bf800f3435c6dbdbe966854f57d0cb10
maddie-moffett <mnrmoffett@gmail.com>
2025-11-05 13:09:54 -0700

    clean up buildcfn and implement the db.filepaths

--- R/BuildClusterFilteredNetwork.R
+++ R/BuildClusterFilteredNetwork.R
@@ -5,10 +5,10 @@
 #' If either of these conditions are not met, then it will be removed from the list of PPI edges.
 #' This new, cluster filtered network is then assigned to the global namespace.
 #'
+#' @param gene.cccn.edges TODO
 #' @param stringdb.edges Data frame of consisting of the network of interactions from the genes of study pulled from the STRINGdb database
 #' @param genemania.edges GeneMANIA network of protein-protein interactions from the genes of study; defaults to NA
 #' @param kinsub.edges TODO
-#' @param gene.cccn.edges TODO
 #' @param db.filepaths A vector of paths to the additional ppi network files; defaults to an empty vector
 #'
 #' @return A list containing the following data structures at the given index:
@@ -17,38 +17,56 @@
 #'
 #' @export
 #' @examples
-#' Example_Output <- BuildClusterFilteredNetwork(ex.stringdb.edges, ex.gm.edges, "need example", "need example", "need example")
+#' Example_Output <- BuildClusterFilteredNetwork("need example", ex.stringdb.edges, ex.gm.edges, "need example", "need example")
 #' utils::head(Example_Output)
 
-BuildClusterFilteredNetwork <- function(stringdb.edges, genemania.edges, kinsub.edges, gene.cccn.edges, db.filepaths = c(), stringdb=TRUE, genemania=TRUE, kinsub=TRUE) {
-  # Give users the option of opting out of one, two, but not three designated PPI datasets
-  if (stringdb == FALSE) {stringdb.edges = NULL}
-  if (genemania == FALSE) {genemania.edges = NULL}
-  if (kinsub == FALSE) {kinsub.edges = NULL}
+BuildClusterFilteredNetwork <- function(gene.cccn.edges, stringdb.edges = NULL, genemania.edges = NULL, kinsub.edges = NULL, db.filepaths = c(NULL)) {
+
   # Combine PPIs from different databases
   # First Normalize Weights
-  if (stringdb == TRUE) {
+  if (!is.null(stringdb.edges)) {
     stringdb.edges$Weight <- 100 * stringdb.edges$Weight / max(stringdb.edges$Weight, na.rm = TRUE) } #  this returns a range of 0 to 100
-  if (genemania == TRUE) {
+  if (!is.null(genemania.edges)) {
     genemania.edges$Weight <- 100 * genemania.edges$Weight / max(genemania.edges$Weight, na.rm = TRUE) } #  this returns a range of 0 to 100
+
   # Note: if additional PPI data is desired, duplicate the normalization of Weights above and add the additional edge file(s) here (and in the function header)
-  # Check that all edge data objects are not NULL
-  if (is.null (c(stringdb.edges, genemania.edges, kinsub.edges))) {
-    print("Warning: no PPI edges found to create cluster filtered network!")}
+  # Done see below
+
   # Combine gathered PPI edges into one data frame
   combined.PPIs <- rbind(stringdb.edges, genemania.edges, kinsub.edges)
-  # Check that this found all the data objects
-  if (!exists(combined.PPIs)) {
-    print("Warning: no PPI edges found to create cluster filtered network!")}
+
+  if(!is.null(db.filepaths)){
+    for(path in db.filepaths){
+      db.edges <- utils::read.table(path)
+      if(colnames(db.edges) != c("source", "target", "interaction", "Weight")){                        # Ensure the column names are the same
+        print("WARNING: incorrect column names in the file " + path + ". This file has been skipped.") # Give warning + skip if not
+        print("Ensure the column names are as follows: source, target, interaction, Weight")
+      } else {
+        db.edges$Weight <- 100 * db.edges$Weight / max(db.edges$Weight, na.rm = TRUE)                  # return a range of 0 to 100
+        combined.PPIs <- rbind(combined.PPIs, db.edges)                                                # add it to combined.PPIs
+      }
+    }
+  }
+
+  # Check that all edge data objects are not NULL
+  if(is.null(combined.PPIs)){
+    print("WARNING: no PPI edges found to create cluster filtered network!")
+    print("Returning the gene.cccn.edges")
+    return(gene.cccn.edges)
+    }
+
   cfn1 <- merge(gene.cccn.edges[,c("source", "target")], combined.PPIs, by=c("source", "target"))
+
   # Undirected edges may be reversed in their order so merge the other way around.
   reversed <- combined.PPIs
   reversed <- reversed[ , c("target", "source", setdiff(names(gene.cccn.edges), c("source", "target")))]
   colnames(reversed)[1:2] <- c("source", "target")  # Rename for merge compatibility
   cfn2 <- merge(reversed[,c("source", "target")], combined.PPIs, by=c("source", "target"))
+
   # Combine both (removing redundant rows if needed)
   cfn <- rbind(cfn1, cfn2)
   cfn <- unique(cfn)
+
   return(list(combined.PPIs, cfn))
 }
 #______________________________________________________________________________________________________________________________
