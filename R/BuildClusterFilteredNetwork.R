9c997c698878417dcaf700dfe8f960c706d2b944
Mark L. Grimes <mark.grimes@mso.umt.edu>
2025-11-05 10:17:37 -0700

    Give users ability to opt out of all three PPI databases, plus additional robustification.

--- R/BuildClusterFilteredNetwork.R
+++ R/BuildClusterFilteredNetwork.R
@@ -4,7 +4,7 @@
 #' All edges in the PPI network are checked against the cocluster correlation network to ensure existence and a non-zero weight.
 #' If either of these conditions are not met, then it will be removed from the list of PPI edges.
 #' This new, cluster filtered network is then assigned to the global namespace.
-#' 
+#'
 #' @param stringdb.edges Data frame of consisting of the network of interactions from the genes of study pulled from the STRINGdb database
 #' @param genemania.edges GeneMANIA network of protein-protein interactions from the genes of study; defaults to NA
 #' @param kinsub.edges TODO
@@ -20,14 +20,21 @@
 #' Example_Output <- BuildClusterFilteredNetwork(ex.stringdb.edges, ex.gm.edges, "need example", "need example", "need example")
 #' utils::head(Example_Output)
 
-BuildClusterFilteredNetwork <- function(stringdb.edges, genemania.edges, kinsub.edges, gene.cccn.edges, db.filepaths = c()) {
+BuildClusterFilteredNetwork <- function(stringdb.edges, genemania.edges, kinsub.edges, gene.cccn.edges, db.filepaths = c(), stringdb=TRUE, genemania=TRUE, kinsub=TRUE) {
+  # Give users the option of opting out of all three designated PPI datasets
+  if (stringdb == FALSE) {stringdb.edges = NULL}
+  if (genemania == FALSE) {genemania.edges = NULL}
+  if (kinsub == FALSE) {kinsub.edges = NULL}
   # Combine PPIs from different databases
   # First Normalize Weights
-  stringdb.edges$Weight <- 100 * stringdb.edges$Weight / max(stringdb.edges$Weight, na.rm = TRUE) #  this returns a range of 0 to 100
-  genemania.edges$Weight <- 100 * genemania.edges$Weight / max(genemania.edges$Weight, na.rm = TRUE) #  this returns a range of 0 to 100
+  if (stringdb == TRUE) {
+    stringdb.edges$Weight <- 100 * stringdb.edges$Weight / max(stringdb.edges$Weight, na.rm = TRUE) } #  this returns a range of 0 to 100
+  if (genemania == TRUE) {
+    genemania.edges$Weight <- 100 * genemania.edges$Weight / max(genemania.edges$Weight, na.rm = TRUE) } #  this returns a range of 0 to 100
   # Note: if additional PPI data is desired, duplicate the normalization of Weights above and add the additional edge file(s) here (and in the function header)
   # Now combine
   combined.PPIs <- rbind(stringdb.edges, genemania.edges, kinsub.edges)
+  if (dim (combined.PPIs == NULL)) {print("Warning: no PPI edges found to create cluster filtered network!")}
   cfn1 <- merge(gene.cccn.edges[,c("source", "target")], combined.PPIs, by=c("source", "target"))
   # Undirected edges may be reversed in their order so merge the other way around.
   reversed <- combined.PPIs
