caa59d21b9ac6aedac71b04e97ea1eb4fedd051c
Gram-S <grantms4455@gmail.com>
2026-04-15 16:08:43 -0600

    Change variable names . -> _

--- tests/testthat/test_makecorrelationnetwork.R
+++ tests/testthat/test_makecorrelationnetwork.R
@@ -3,24 +3,24 @@
 test_that("MakeCorrelationNetwork() gives right answer", {
     # first we have to run MakeClusterList to get the inputs for MakeCorrelationNetwork
     set.seed(88)
-    clusterlist.data <- MakeClusterList(ex_tiny_ptm_table, keeplength = 2, toolong = 3.5)
-    common.clusters <- clusterlist.data[[1]]
-    adj.consensus <- clusterlist.data[[2]]
-    ptm.correlation.matrix <- clusterlist.data[[3]]
+    clusterlist_data <- MakeClusterList(ex_tiny_ptm_table, keeplength = 2, toolong = 3.5)
+    common_clusters <- clusterlist_data[[1]]
+    adj_consensus <- clusterlist_data[[2]]
+    ptm_correlation_matrix <- clusterlist_data[[3]]
 
     # now run MakeCorrelationNetwork
     correlationnetwork.data <- MakeCorrelationNetwork(adj.consensus, ptm.correlation.matrix)
-    ptm.cccn.edges <- correlationnetwork.data[[1]]
-    gene.cccn.edges <- correlationnetwork.data[[2]]
-    gene.cccn.nodes <- correlationnetwork.data[[3]]
+    ptm_cccn_edges <- correlationnetwork_data[[1]]
+    gene_cccn_edges <- correlationnetwork_data[[2]]
+    gene_cccn_nodes <- correlationnetwork_data[[3]]
 
     # check some values in the PTM CCCN edge list
-    expect_equal(nrow(ptm.cccn.edges),  1116)
-    expect_equal(nrow(gene.cccn.edges),  473)
-    expect_equal(length(gene.cccn.nodes),  96)
-    expect_equal(sum(gene.cccn.edges$Weight),  18.94286, tolerance = 1e-6)
-    expect_equal(sum(ptm.cccn.edges$Weight),  76.24286, tolerance = 1e-6)
+    expect_equal(nrow(ptm_cccn_edges),  1116)
+    expect_equal(nrow(gene_cccn_edges),  473)
+    expect_equal(length(gene_cccn_edges),  96)
+    expect_equal(sum(gene_cccn_edges$Weight),  18.94286, tolerance = 1e-6)
+    expect_equal(sum(ptm_cccn_edges$Weight),  76.24286, tolerance = 1e-6)
 
-    expect_equal(ptm.cccn.edges[ptm.cccn.edges$source == "RBM12B p Y326" & ptm.cccn.edges$target == "SRCIN1 p Y264", "Weight"], 0.6785714, tolerance = 1e-6)
-    expect_equal(gene.cccn.edges[gene.cccn.edges$source == "CDK1" & gene.cccn.edges$target == "HSP90AB4P", "Weight"], -0.2142857, tolerance = 1e-6)
+    expect_equal(ptm_cccn_edges[ptm_cccn_edges$source == "RBM12B p Y326" & ptm_cccn_edges$target == "SRCIN1 p Y264", "Weight"], 0.6785714, tolerance = 1e-6)
+    expect_equal(gene_cccn_edges[gene_cccn_edges$source == "CDK1" & gene_cccn_edges$target == "HSP90AB4P", "Weight"], -0.2142857, tolerance = 1e-6)
 })
