ead553b2053f38e88750bcde1a62e64be19681f1
Lucy <lgw2@uw.edu>
2026-05-15 21:59:57 -0600

    Make first half of vignette run

---
+++ data-raw/processed-brca.R
@@ -0,0 +1,22 @@
+file_path <- system.file("extdata", "PhosphoDiscoData_mmc9.txt", package = "PTMsToPathways")
+newphos <- utils::read.table(file_path, header = TRUE,
+                               stringsAsFactors = FALSE, sep = "\t", comment.char = "#",
+                               na.strings = "", quote = "", fill = TRUE)
+ newphos$Amino.Acid <- sapply(newphos$variable_sites_names, function(x) substring (x, 1, 1))
+ newphos$Site <- trimws(substring(newphos$variable_sites_names, 2))
+ newphos$Site <- sub("[a-z]$", "", newphos$Site)
+  newphos$Peptide.Name <- mapply(
+   name.peptide, genes = newphos$gene_symbol,
+   sites =  newphos$Site, aa = newphos$Amino.Acid)
+phosdata <- newphos[, 3:ncol(newphos), ]
+rownames(phosdata) <- newphos$Peptide.Name
+phosdata <- phosdata[, !(names(phosdata) %in% c("gene_symbol", "variable_sites_names", "Amino.Acid", "Site", "Peptide.Name"))]
+ptmtable <- phosdata
+set.seed(88)
+brca_clusterlist_data <- MakeClusterList(ptmtable,
+                                    keeplength = 3, toolong = 3.5)
+brca_CCCN_data <- MakeCorrelationNetwork(adj.consensus.matrix,
+                                    ptm.correlation.matrix)
+
+usethis::use_data(brca_clusterlist_data, overwrite = TRUE)
+usethis::use_data(brca_CCCN_data, overwrite = TRUE)
