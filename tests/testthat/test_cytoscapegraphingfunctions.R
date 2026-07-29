9c8268e9331d60da691254f7b64c0494bb84e3f0
Gram-S <grantms4455@gmail.com>
2026-07-29 17:34:54 -0600

    Change up variable names to make them more specific and fix strip.cy.goo test

--- tests/testthat/test_cytoscapegraphingfunctions.R
+++ tests/testthat/test_cytoscapegraphingfunctions.R
@@ -5,14 +5,14 @@ test_that("filter.edges.0 function gives right answer", {
   
   nodenames <- c('Axon Guidance', 'Lipid and lipoprotein metabolism', 'Validated nuclear estrogen receptor alpha network', 'Vitamin B12 metabolism')
   
-  filtered <- filter.edges.0(nodenames, ex_pathway_crosstalk_network)
-  dim_filtered <- dim(filtered)
+  edges_0_output <- filter.edges.0(nodenames, ex_pathway_crosstalk_network)
+  dim_edges_0_output <- dim(edges_0_output)
   
   # Are dimensions correct?
-  expect_equal(dim_filtered, c(6, 4))
+  expect_equal(dim_edges_0_output, c(6, 4))
   # Is a random weight correct?
-  expect_equal(filtered[4,3], "0.0128205128205128")
-  expect_equal(filtered[1,3], "0.3")
+  expect_equal(edges_0_output[4,3], "0.0128205128205128")
+  expect_equal(edges_0_output[1,3], "0.3")
   
 })
 
@@ -20,14 +20,14 @@ test_that("filter.edges.1 function gives right answer", {
   
   nodenames <- c('SYK p S295', 'NOL8 p S723', 'EGFR p Y1197')  
   
-  filtered <- filter.edges.1(nodenames, ex_ptm_cccn_edges)
-  dim_filtered <- dim(filtered)
+  edges_1_output <- filter.edges.1(nodenames, ex_ptm_cccn_edges)
+  dim_edges_1_output <- dim(edges_1_output)
   
   # Are dimensions correct?
-  expect_equal(dim_filtered, c(73, 4))
+  expect_equal(dim_edges_1_output, c(73, 4))
   # Is a random weight correct?
-  expect_equal(filtered[50,3], 0.5)
-  expect_equal(filtered[10,3], -0.294117647)
+  expect_equal(edges_1_output[50,3], 0.5)
+  expect_equal(edges_1_output[10,3], -0.294117647)
   
 })
 
@@ -35,38 +35,42 @@ test_that("filter.edges.between function gives right answer", {
   
   nodes1 <- c('KIRREL1', 'EPHA2', 'CTTN')  
   nodes2 <- c('EGFR', 'LCK', 'ERBIN')  
-  filtered <- filter.edges.between(nodes1, nodes2, ex_cfn)
-  dim_filtered <- dim(filtered)
+  
+  filter_edges_output <- filter.edges.between(nodes1, nodes2, ex_cfn)
+  dim_filter_edges_output <- dim(filter_edges_output)
   
   # Are dimensions correct?
-  expect_equal(dim_filtered, c(4, 4))
+  expect_equal(dim_filter_edges_output, c(4, 4))
   # Is a random weight correct?
-  expect_equal(filtered[2, 4], 24.54212, tolerance = 1e-6)
-  expect_equal(filtered[4, 4], 2.4542125, tolerance = 1e-6)
+  expect_equal(filter_edges_output[2, 4], 24.54212, tolerance = 1e-6)
+  expect_equal(filter_edges_output[4, 4], 2.4542125, tolerance = 1e-6)
   
 })
 
 test_that("connectNodes.all function gives right answer", {
   
   nodes <- c('LCK', 'ERBIN')  
-  filtered <- connectNodes.all(nodes, ig.graph=NULL, edgefile=ex_cfn, newgraph=TRUE)
-  dim_filtered <- dim(filtered)
+  connectNodes_output <- connectNodes.all(nodes, ig.graph=NULL, edgefile=ex_cfn, newgraph=TRUE)
+  dim_connectNodes_output <- dim(connectNodes_output)
   
   # Are dimensions correct?
-  expect_equal(dim_filtered, c(2, 4))
+  expect_equal(dim_connectNodes_output, c(2, 4))
   # Is a random weight correct?
-  expect_equal(filtered[1, 4], 13.260073, tolerance = 1e-6)
-  expect_equal(filtered[2, 4], 13.992674, tolerance = 1e-6)
+  expect_equal(connectNodes_output[1, 4], 13.260073, tolerance = 1e-6)
+  expect_equal(connectNodes_output[2, 4], 13.992674, tolerance = 1e-6)
   
 })
 
 test_that("getCyEdgeNames function gives right answer", {
   
-  filtered <- getCyEdgeNames(ex_pathway_crosstalk_network)
+  CyEdgeNames_output <- getCyEdgeNames(ex_pathway_crosstalk_network)
+  
+  # Is the amount of edges correct?
+  expect_equal(length(CyEdgeNames_output), 37)
 
   # Is a random edge name string correct?
-  expect_equal(filtered[[16]], 'RXR/VDR pathway (PTM_cluster_evidence) Validated nuclear estrogen receptor alpha network')
-  expect_equal(filtered[[30]], 'Lipid and lipoprotein metabolism (pathway_Jaccard_similarity) Vitamin B12 metabolism')
+  expect_equal(CyEdgeNames_output[[16]], 'RXR/VDR pathway (PTM_cluster_evidence) Validated nuclear estrogen receptor alpha network')
+  expect_equal(CyEdgeNames_output[[30]], 'Lipid and lipoprotein metabolism (pathway_Jaccard_similarity) Vitamin B12 metabolism')
   
 })
 
