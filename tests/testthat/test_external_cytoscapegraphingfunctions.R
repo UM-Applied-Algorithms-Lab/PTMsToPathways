81cdc6c6a8c2c9c0be8a5a95e2eea9fd645a20aa
Gram-S <grantms4455@gmail.com>
2026-08-06 15:24:40 -0600

    minor fixes

--- tests/testthat/test_external_cytoscapegraphingfunctions.R
+++ tests/testthat/test_external_cytoscapegraphingfunctions.R
@@ -1,12 +1,12 @@
 # For testing functions in cytoscapegraphing which require cytoscape open
 
 # Conditional. Skip all tests if cytoscapePing() returns an error class -> Conclude cytoscape is not open. 
-testthat::skip_if(inherits(try(suppressMessages(RCy3::cytoscapePing()), silent=TRUE), what="try-error"), message="Could not ping Cytoscape. Likely because the application is not open.")
+testthat::skip_if( inherits(try(suppressMessages(RCy3::cytoscapePing()), silent=TRUE), what="try-error"), message="Could not ping Cytoscape. Likely because the application is not open.")
 
 test_that( "cytoscape.graph.PCN.pathways function gives right answer", {
   
   # Takes a VERY long time
-  cytoscape.graph.PCN.pathways(ex_pathway_crosstalk_network, "EXAMPLE DATA PCN")
+  suppressMessages(cytoscape.graph.PCN.pathways(ex_pathway_crosstalk_network, "EXAMPLE DATA PCN"))
   
   # Does the Selenium pathway node exist?
   RCy3::selectNodes('Selenium pathway','name')
@@ -18,7 +18,7 @@ test_that( "cytoscape.graph.PCN.pathways function gives right answer", {
   neighbor_names <- RCy3::getSelectedNodes()
   
   # Does the Selenium pathway node have the correct amount of neighbors?
-  expect_true(length(neighbor_names) == 5)
+  expect_equal(length(neighbor_names), 5)
   
   # Does the Selenium pathway node have specific neighbors?
   exp_neighbors <-  c("Vitamin B12 metabolism", "RXR/VDR pathway")
